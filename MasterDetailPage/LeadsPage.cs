562ca82a3766aca6d2087bed197f42f10b8e1404
varunpsr <varunrathorenator@gmail.com>
2015-03-09 14:19:58 +0530

    First Commit: xamarin forms

---
+++ MasterDetailPage/LeadsPage.cs
@@ -0,0 +1,17 @@
+using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+
+namespace MasterDetailPage
+{
+	public class LeadsPage : ContentPage
+	{
+		public LeadsPage ()
+		{
+			Title = "Leads";
+			Icon = "Leads.png";
+		}
+	}
+
+}
+
