d1301d825673e146d51bb43d97a5267c6469dffc
varunpsr <varunrathorenator@gmail.com>
2015-03-09 14:35:19 +0530

    First Commit: xamarin-forms

---
+++ MasterDetailPage/MenuItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+
+namespace MasterDetailPage
+{
+	public class MenuItem
+	{
+		public string Title { get; set; }
+
+		public string IconSource { get; set; }
+
+		public Type TargetType { get; set; }
+	}
+}
+
